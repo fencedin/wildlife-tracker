e621fa85ccb841079301e1025d6a49755329bad8
vincent rotter & Natalie Blackburn <student@epicodus.com>
2014-04-01 10:43:17 -0700

    add update and destroy functionality to Species class

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,6 +1,42 @@
 class SpeciesController < ApplicationController
   def index
-    @species = Species.all
-    render('species/index.html.erb')
+    @species_all = Species.all
+    @species = Species.new
+    render 'species/index.html.erb'
+  end
+
+  def create
+    @species_all = Species.all
+    @species = Species.new(:name =>params[:name])
+    if @species.save
+      render 'species/index.html.erb'
+    else
+      render 'species/index.html.erb'
+    end
+  end
+
+  def edit
+    @species = Species.find(params[:id])
+    render 'species/edit.html.erb'
+  end
+
+  def update
+    @species_all = Species.all
+    @species = Species.find(params[:id])
+    @species.update(:name =>params[:name])
+    if @species.save
+      render 'species/index.html.erb'
+    else
+      render 'species/edit.html.erb'
+    end
+  end
+
+  def destroy
+    @species_all = Species.all
+    @species = Species.find(params[:id])
+    @species.destroy
+    flash.alert = "Species deleted!"
+    flash.notice = "Species destroyed!"
+    render 'species/index.html.erb'
   end
 end
